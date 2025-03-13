0c03c8fbf6c4dceda6de51c5e88e1d80feaac5de
Mo Zaabat <mzaabat@hotmail.com>
2025-03-13 14:34:05 +0100

    tilføjet ir sensor

--- ev3.py
+++ ev3.py
@@ -2,11 +2,10 @@
 
 from time import sleep
 from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent
-from ev3dev2.sensor.lego import UltrasonicSensor
-from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sensor.lego import UltrasonicSensor, ColorSensor, InfraredSensor 
 us = UltrasonicSensor()
-
 colorsensor = ColorSensor()
+ir = InfraredSensor()
 
 # Opsæt de store motorer på OUTPUT_A og OUTPUT_B
 motor_a = LargeMotor(OUTPUT_A)
@@ -55,16 +54,21 @@ motor_c.off()
 while True:
     distance = us.distance_centimeters
     reflected_light = colorsensor.reflected_light_intensity
+    ir_distance = ir.proximity 
     # Bruger ultralydssensor til at mærke om der er vægge 
     if distance < 10 :
         motor_a.on(SpeedPercent(-50))
         motor_b.on(SpeedPercent(50))
-        sleep(1)
+        sleep(2)
 
     # Bruger farvesensor til at aflæse om der er en bold der er taget
     elif reflected_light > 80 :                        # Belysning på meget lys farve (bold er højst sandsynligt meget lys eller hvid)
         motor_c.on_for_degrees(SpeedPercent(60), 45)   # Frem
         motor_c.on_for_degrees(SpeedPercent(-60), 45)  # Tilbage
-       
-
+    
+    #Bruger IR sensor til at aflæse om der er en 
+    if ir_distance < 4 :
+        motor_a.off()
+        motor_b.off() 
+        sleep(2)
 
