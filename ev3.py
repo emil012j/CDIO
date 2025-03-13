55b119c915d9373002f4b751a844ef68843be7dc
Mo Zaabat <mzaabat@hotmail.com>
2025-03-13 14:40:41 +0100

    små rettelse i ev3 filen

--- ev3.py
+++ ev3.py
@@ -14,42 +14,6 @@ motor_b = LargeMotor(OUTPUT_B)
 # Opsæt mellem motoren (armen) på OUTPUT_C
 motor_c = MediumMotor(OUTPUT_C)
 
-# Start med at køre hurtigt fremad i 1,5 sekunder
-motor_a.on(SpeedPercent(100))
-motor_b.on(SpeedPercent(100))
-sleep(3)
-
-# Drej skarpt til højre
-motor_a.on(SpeedPercent(100))
-motor_b.on(SpeedPercent(-50))
-sleep(1)
-
-# Bak hurtigt tilbage
-motor_a.on(SpeedPercent(-80))
-motor_b.on(SpeedPercent(-80))
-sleep(1)
-
-# Drej rundt på stedet
-motor_a.on(SpeedPercent(100))
-motor_b.on(SpeedPercent(-100))
-sleep(1)
-
-# Stop de store motorer
-motor_a.off()
-motor_b.off()
-
-# Kør armen en halv omgang opad
-motor_c.on_for_degrees(SpeedPercent(75), 90)
-
-# Vift armen frem og tilbage hurtigt 3 gange 
-for _ in range(3):
-    motor_c.on_for_degrees(SpeedPercent(60), 45)   # Frem
-    motor_c.on_for_degrees(SpeedPercent(-60), 45)  # Tilbage
-
-# Stop mellem motoren og gå til neutral position
-motor_c.on_for_degrees(SpeedPercent(-50), 45)
-motor_c.off()
-
 
 while True:
     distance = us.distance_centimeters
