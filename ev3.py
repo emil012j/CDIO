8f27202630ef20088506ef91099e99789912968f
Mo Zaabat <mzaabat@hotmail.com>
2025-03-13 13:44:34 +0100

    tilføjet flere ultralyd og farvesensor

--- ev3.py
+++ ev3.py
@@ -2,6 +2,11 @@
 
 from time import sleep
 from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent
+from ev3dev2.sensor.lego import UltrasonicSensor
+from ev3dev2.sensor.lego import ColorSensor
+us = UltrasonicSensor()
+
+colorsensor = ColorSensor()
 
 # Opsæt de store motorer på OUTPUT_A og OUTPUT_B
 motor_a = LargeMotor(OUTPUT_A)
@@ -45,3 +50,21 @@ for _ in range(3):
 # Stop mellem motoren og gå til neutral position
 motor_c.on_for_degrees(SpeedPercent(-50), 45)
 motor_c.off()
+
+
+while True:
+    distance = us.distance_centimeters
+    reflected_light = colorsensor.reflected_light_intensity
+    # Bruger ultralydssensor til at mærke om der er vægge 
+    if distance < 10 :
+        motor_a.on(SpeedPercent(-50))
+        motor_b.on(SpeedPercent(50))
+        sleep(1)
+
+    # Bruger farvesensor til at aflæse om der er en bold der er taget
+    elif reflected_light > 80 :                        # Belysning på meget lys farve (bold er højst sandsynligt meget lys eller hvid)
+        motor_c.on_for_degrees(SpeedPercent(60), 45)   # Frem
+        motor_c.on_for_degrees(SpeedPercent(-60), 45)  # Tilbage
+       
+
+
