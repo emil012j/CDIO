be10ce4525ece07516a2d73e41582301c08319f9
Mo Zaabat <mzaabat@hotmail.com>
2025-03-27 14:02:28 +0100

    infrared test in ev3 file

---
+++ tests/sensor/infrared_sensor.py
@@ -0,0 +1,12 @@
+from time import sleep
+from ev3dev2.sensor.lego import InfraredSensor
+
+# Initialize the sensor
+
+ir = InfraredSensor()
+
+while True:
+    closeness = ir.proximity    #Get the closeness, 0 for very close and 100 for very far
+    print("Closeness: ".format(closeness))
+    sleep(0.5)
+
