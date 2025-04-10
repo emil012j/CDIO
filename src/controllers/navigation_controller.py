302cd74801d8a18432b5ac6a26c052abacc38427
Mo Zaabat <mzaabat@hotmail.com>
2025-04-10 14:41:28 +0200

    navigation implementering

--- src/controllers/navigation_controller.py
+++ src/controllers/navigation_controller.py
@@ -1,1 +1,40 @@
-# Kode til hvordan robotten ved hvor den skal navigere hen
+# Kode til hvordan robotten ved hvor den skal navigere hen (Eksempel)
+
+import cv2
+import numpy as np 
+import time 
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B
+
+# Tænd motor
+motor_a = LargeMotor(OUTPUT_A)
+motor_b = LargeMotor(OUTPUT_B)
+
+# Kamera 
+cap = cv2.VideoCapture(0)
+
+def moveRobot(direction):
+    if direction == "FORWARD":
+        motor_a.on(50)
+        motor_b.on(50)
+    elif direction == "BACKWARD":
+        motor_a.on(-50)
+        motor_b.on(-50)
+    elif direction ==  "LEFT":
+        motor_a.on(30)
+        motor_b.on(-30)
+    elif direction == "RIGHT":
+        motor_a.on(-30)
+        motor_b.on(30)
+    
+    time.sleep(1)
+    motor_a.off()
+    motor_b.off
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    
