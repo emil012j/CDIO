ee7263bca5db9ba6a4a6874faf46b2fd21217991
AsgerFrandsen <asgerfrandsen@gmail.com>
2025-06-03 10:55:02 +0200

    ny server fil på robot

--- tests/robot/ev3_server.py
+++ tests/robot/ev3_server.py
@@ -2,7 +2,7 @@
 import socket
 import json
 import threading
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, SpeedPercent
 from time import sleep
 
 # Motorer
@@ -52,32 +52,47 @@ def handle_command_server():
             if not data:
                 continue
             cmd = json.loads(data.decode())
-            direction = cmd.get("direction")
-            distance = float(cmd.get("distance", 0))
-            move_robot(direction, distance)
+            command = cmd.get("command")
+
+            if command == "forward":
+                distance = float(cmd.get("distance", 0))
+                move_robot_forward(distance)
+            elif command == "turn":
+                angle = float(cmd.get("angle", 0))
+                move_robot_turn(angle)
+            elif command == "stop":
+                motor_a.off()
+                motor_d.off()
         except Exception as e:
             print("Fejl i kommando:", e)
         finally:
             conn.close()
 
-# Bevaegelsesfunktion
-def move_robot(direction, distance_cm):
-    speed = 10  # cm/s
-    duration = distance_cm / speed
-    print("koerer {} i {} sekunder ({} cm)".format(direction, duration, distance_cm))
+# Bevaegelsesfunktioner
+def move_robot_forward(distance_cm):
+    speed_cm_s = 10
+    duration = distance_cm / speed_cm_s
+    print("Koerer frem i {:.2f} sekunder ({} cm)".format(duration, distance_cm))
+    motor_a.on(-50)
+    motor_d.on(-50)
+    sleep(duration)
+    motor_a.off()
+    motor_d.off()
 
-    if direction == "FORWARD":
-        motor_a.on(50)
-        motor_d.on(50)
-    elif direction == "BACKWARD":
-        motor_a.on(-50)
-        motor_d.on(-50)
-    elif direction == "LEFT":
-        motor_a.on(-30)
-        motor_d.on(30)
-    elif direction == "RIGHT":
+def move_robot_turn(angle_deg):
+    turn_time_per_degree = 1.0 / 90  # 90 degrees = 1 second
+    duration = abs(angle_deg) * turn_time_per_degree
+    direction = 'venstre' if angle_deg > 0 else 'højre'
+    print("Drejer {} {} grader i {:.2f} sekunder".format(direction, angle_deg, duration))
+
+    if angle_deg > 0:
+        # Turn left
         motor_a.on(30)
         motor_d.on(-30)
+    else:
+        # Turn right
+        motor_a.on(-30)
+        motor_d.on(30)
 
     sleep(duration)
     motor_a.off()
@@ -93,3 +108,4 @@ if __name__ == "__main__":
     # Hold main thread i live
     while True:
         sleep(1)
+
