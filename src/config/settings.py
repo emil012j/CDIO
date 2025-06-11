5737e3598a829630ca72b7019fb36b876d734aff
Mo Zaabat <mzaabat@hotmail.com>
2025-06-11 11:20:28 +0200

    implementeret farvesensor

--- src/config/settings.py
+++ src/config/settings.py
@@ -43,5 +43,5 @@ MAX_FORWARD_DISTANCE = 30 # maks længde for fremadkørsel
 
 
 ROBOT_TURN_SPEED = 40 # Motor hastighed til drejning
-ROBOT_FORWARD_SPEED = 30 # Motor hastighed til fremadkørsel  
+ROBOT_FORWARD_SPEED = 20 # Motor hastighed til fremadkørsel  
 ESTIMATED_TURN_RATE = 130.0  # grader per sekund - til konvertering af duration til vinkel
