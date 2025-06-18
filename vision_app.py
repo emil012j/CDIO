9f2d6ab0f3e4c015a03923fedee7383a53ece576
Mo Zaabat <mzaabat@hotmail.com>
2025-06-18 11:55:58 +0200

    cross avoidance being handled anytime

--- vision_app.py
+++ vision_app.py
@@ -93,11 +93,14 @@ def main():
                 cross_x, cross_y = cross_pos
                 dist_to_cross = ((robot_center[0] - cross_pos[0]) ** 2 + (robot_center[1] - cross_pos[1]) ** 2) ** 0.5
                 if dist_to_cross < 100:  # Adjust threshold as needed
-                    print("*** CLOSE TO CROSS - GOING BACKWARDS AND TURNING ***")
-                    commander.send_stop_command()
-                    commander.send_backward_command(10)
-                    commander.send_turn_command("left", 1.0)
-                    continue
+                    if commander.can_send_command():
+                        print("*** CLOSE TO CROSS - GOING BACKWARDS AND TURNING ***")
+                        commander.send_stop_command()
+                        commander.send_turn_command("left", 10.0)
+                        commander.send_forward_command(distance=10)
+                        continue
+                    else:
+                        current_state = ROUTE_PLANNING
             # --- End cross avoidance logic ---
 
             if robot_head and robot_tail:
